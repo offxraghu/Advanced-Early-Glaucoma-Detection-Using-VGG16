ad0881941050bb189982478b78d84b64a37318ce
G Raghavendra <147441167+offxraghu@users.noreply.github.com>
2025-07-10 00:17:03 +0530

    Create header.php

---
+++ Glaucoma (Frontend)/Include/header.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><i class="fas fa-eye me-2"></i>Glaucoma Detection</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link active" href="dashboard.php"><i class="fas fa-tachometer-alt me-2"></i>Dashboard</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.php"><i class="fas fa-info-circle me-2"></i>About</a></li>
+                <li class="nav-item"><a class="nav-link" href="view-report.php"><i class="fas fa-file-alt me-2"></i>View Report</a></li>
+                <li class="nav-item"><a class="nav-link" href="http://127.0.0.1:5001/" target="_blank"><i class="fas fa-search me-2"></i>Detect</a></li>
+                <li class="nav-item"><a class="nav-link" href="change-password.php"><i class="fas fa-key me-2"></i>Change Password</a></li>
+                <li class="nav-item"><a class="nav-link text-danger" href="include/logout.php"><i class="fas fa-sign-out-alt me-2"></i>Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
